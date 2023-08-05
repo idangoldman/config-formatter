3e3455702e308ef35c7f069b9d0b808a4538d8e2
Idan Goldman <idan@123code.co>
2023-08-06 01:57:54 +0300

    Update jsonnet.js

--- formats/jsonnet.js
+++ formats/jsonnet.js
@@ -1,9 +1,9 @@
 import { Jsonnet } from '@hanazuki/node-jsonnet';
 
-export function readJSONNET(content) {
+export function parse(content) {
   return (new Jsonnet()).evaluateSnippet(content);
 }
 
-export function writeJSONNET(content) {
+export function convert(content) {
   return JSON.stringify(content, null, 2).replace(/"([^"]+)":/g, '$1:');
 }
