b35cb2dbcab9de934fb362e29bced30dbefb6d56
Idan Goldman <idan@123code.co>
2023-08-09 00:18:56 +0300

    moved from commander to yargs

--- bin/index.js
+++ bin/index.js
@@ -3,26 +3,34 @@
 // Examples of usage:
 // stooge -i tests/fixtures/fixture.yaml -o tests/tmp/yaml-to.json -f json
 
-import { Command } from 'commander';
-import packageJson from '#root/package.json' assert { type: "json" };
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
 
 import { SUPPORTED_EXTENSIONS } from '#root/lib/supported.js';
 import { performConversion } from '#root/lib/conversion.js';
 
-const program = new Command();
+const argv = yargs(hideBin(process.argv))
+  .usage('Usage: stooge --input <input> --output <output> --format <format>')
+  .option('input', {
+    alias: ['i', 'input'],
+    describe: 'Path to the single or multiple input files to be converted.',
+    type: 'string'
+  })
+  .option('output', {
+    alias: ['o', 'output'],
+    describe: 'Path to the output file or folder for saving the converted content.',
+    type: 'string'
+  })
+  .option('format', {
+    alias: ['f', 'format'],
+    choices: SUPPORTED_EXTENSIONS,
+    default: 'yml',
+    describe: `Format seting of the output file, supported formats: ${SUPPORTED_EXTENSIONS.join(', ')}.`,
+    type: 'string',
+  })
+  .argv;
 
-program
-  .option('-i, --input <input>', 'input file')
-  .option('-o, --output <output>', 'output file')
-  .option('-of, --output-folder <output>', 'output file folder.', 'same as the input file')
-  .option('-f, --format <format>', `Convert into one of the supported formats: ${SUPPORTED_EXTENSIONS.join(', ')}.`, 'yml')
-  .version(packageJson.version, '-v, --version', 'Show the installed version')
-  .parse(process.argv);
-
-// Get the values of program arguments
-const { input, output, format } = program;
-
-// Call the conversion function with the program arguments
-performConversion(input, output, format);
+const { input, output, format } = argv;
+await performConversion(input, output, format);
 
 process.exit(0);
